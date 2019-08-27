edebc65143bd5974d4033871d0dea7901ad7f8e7
chancyj <clarence.chan@mail.hsa.net>
2019-08-27 18:52:49 -0400

    Styling(Footer) -- Restructured footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import { Container } from './styles'
-import SiteMap from './components/SiteMap'
-import SocialMediaBlock from './components/SocialMediaBlock'
+import React from "react";
+import { Container } from "./styles";
+import SiteMap from "./components/SiteMap";
 
 const Footer = () => (
   <Container>
     <SiteMap />
-    <SocialMediaBlock />
   </Container>
-)
+);
 
-export default Footer
+export default Footer;
