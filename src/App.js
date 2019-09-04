ddc06c19300a49581497ff20332aa895031dcc0b
chancyj <clarence.chan@mail.hsa.net>
2019-09-03 23:32:50 -0400

    Styling(Main App) -- Removed unnecessary components from main app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import { ThemeProvider } from "styled-components";
-import localStore from "store";
 import theme from "./theme";
 import client from "./client";
 import Home from "./containers/Home/";
 import WrappedSignIn from "./containers/SignIn/";
 import { Footer, NavBar } from "./components";
 import Dashboard from "./containers/Dashboard";
-
-const isSignedIn = async () => {
-  const token = await localStore.get("admin");
-  return token !== null;
-};
 
 const options = {
   position: positions.BOTTOM_CENTER,
@@ -25,18 +19,7 @@ const options = {
 };
 
 class App extends Component {
-  state = {
-    signedIn: false
-  };
-
-  componentDidMount = () => {
-    isSignedIn()
-      .then(res => this.setState({ signedIn: res, loading: false }))
-      .catch(() => alert("An error occurred"));
-  };
-
   render() {
-    const { signedIn } = this.state;
     return (
       <Router>
         <ThemeProvider theme={theme}>
