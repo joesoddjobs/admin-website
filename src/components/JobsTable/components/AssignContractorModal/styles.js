e5658dc49d8631b61074c97057961b87b31ce71a
chancyj <clarence.chan@mail.hsa.net>
2019-09-15 19:42:59 -0400

    Styling(Assign Contractor Modal) -- Created assign contractor modal

---
+++ src/components/JobsTable/components/AssignContractorModal/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Flex } from "@rebass/grid";
+
+const ModalContainer = styled(Flex)`
+  flex-direction: column;
+  height: auto;
+  justify-content: space-between;
+`;
+
+export default ModalContainer;
