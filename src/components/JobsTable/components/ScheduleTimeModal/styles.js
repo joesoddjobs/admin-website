aa5e73aa3459f3646dc5a0152226e9c44a2325c9
chancyj <clarence.chan@mail.hsa.net>
2019-09-15 22:14:10 -0400

    Styling(ScheduleTime Modal) -- Created schedule time modal

---
+++ src/components/JobsTable/components/ScheduleTimeModal/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Flex } from "@rebass/grid";
+
+const ModalContainer = styled(Flex)`
+  flex-direction: column;
+  height: 450px;
+  justify-content: space-between;
+`;
+
+export default ModalContainer;
